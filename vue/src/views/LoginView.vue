0fb7f097d5785afcbd20f8cb67071c09fb4bf703
Heaven Pina <hpina1124@gmail.com>
2024-02-14 15:57:16 -0500

    sign in finished

--- vue/src/views/LoginView.vue
+++ vue/src/views/LoginView.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="login">
     <form v-on:submit.prevent="login">
-      <h1 >Please Sign In</h1>
+      <h1  style="font-family: 'mont'; text-decoration: underline; ">Please Sign In</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
       <div role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
-      <div class="form-input-group">
-        <label for="username">Username</label>
+     <h4><div class="form-input-group">
+        <label for="username">Username:</label>
         <input type="text" id="username" v-model="user.username" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
+      </div></h4>
+      <h4><div class="form-input-group">
+        <label for="password">Password:</label>
         <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <button type="submit">Sign in</button>
-      <p>
+      </div></h4>
+      <button class="sign-btn" type="submit">Sign in</button>
+      <p class="link">
       <router-link v-bind:to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
     </form>
   </div>
@@ -61,9 +61,73 @@ export default {
 </script>
 
 <style scoped>
-.form-input-group {
-  margin-bottom: 1rem;
+
+@font-face {
+  font-family: 'mont';
+  src: url('../assets/fonts/MontereyFLF.ttf');
 }
+@font-face {
+  font-family: 'roboto';
+  src: url('../assets/fonts/Roboto-Regular.ttf');
+}
+
+
+#login{
+  border: 5px solid #dbdbdb; 
+  border-radius: 7px;
+  background-color: rgb(255, 252, 244);
+  max-width: 400px; 
+  max-height: 700px; 
+  margin: auto; 
+  text-align: center;
+  padding: 20px; 
+  box-sizing: border-box;
+  box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.1);
+
+}
+
+.form-input-group {
+  
+  margin-bottom: 1rem;
+  font-size: 15px;
+
+}
+
+.sign-btn {
+  font-size: 1.1em;
+        font-family: 'mont';
+        padding: 10px 15px;
+        margin: 10px;
+        background-color: #893222;
+        color: #dbbe4b;
+        border: none;
+        cursor: pointer;
+        outline: none;
+        cursor: pointer;
+        position: relative;
+        box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+        transition: transform 0.3s;
+        border-radius: 50px;
+
+}
+
+.sign-btn:hover {
+        background-color: #6d271b;
+        transform: scale(1.2);
+}
+
+.link:hover{  
+  color: #893222;
+  text-decoration: underline;
+  transition: transform 0.3s;
+  transform: scale(1.1);
+
+
+}
+
+
+
 label {
   margin-right: 0.5rem;
 }
