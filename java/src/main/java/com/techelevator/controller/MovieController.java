6ccd09968cb8c0a7ff1d2de098eecbe0198987be
Kevin Neel <kevin.neel2@gmail.com>
2024-02-18 13:48:15 -0500

    code cleanup, removed unused classes from API tests

--- java/src/main/java/com/techelevator/controller/MovieController.java
+++ java/src/main/java/com/techelevator/controller/MovieController.java
@@ -9,7 +9,9 @@ import com.techelevator.exception.DaoException;
 import com.techelevator.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
